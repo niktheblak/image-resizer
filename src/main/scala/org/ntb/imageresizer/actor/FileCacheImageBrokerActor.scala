19cfef5c46843864c91c54f24533cfa9ff1a2eb2
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2012-11-19 12:28:16 +0200

    Merged ActorImageDownloader code to FileCacheImageBrokerActor because this code cannot really be reused

--- src/main/scala/org/ntb/imageresizer/actor/FileCacheImageBrokerActor.scala
+++ src/main/scala/org/ntb/imageresizer/actor/FileCacheImageBrokerActor.scala
@@ -3,26 +3,31 @@ package org.ntb.imageresizer.actor
 import java.io.File
 import java.net.URI
 import java.util.concurrent.TimeoutException
+import org.ntb.imageresizer.actor.BaseDownloadActor._
 import org.ntb.imageresizer.cache.TempFileCacheProvider
 import org.ntb.imageresizer.imageformat._
 import org.ntb.imageresizer.resize.UnsupportedImageFormatException
+import org.ntb.imageresizer.actor.ResizeActor._
 import akka.actor.Actor
 import akka.actor.ActorLogging
 import akka.actor.Status
 import akka.actor.actorRef2Scala
 import akka.dispatch.Await
+import akka.dispatch.Future
+import akka.pattern.ask
 import akka.util.Timeout
 import akka.util.duration.intToDurationInt
 import org.apache.http.HttpException
 
 class FileCacheImageBrokerActor extends Actor
   with ActorLogging
-  with ActorImageDownloader
   with TempFileCacheProvider[(URI, Int)]
   with ActorNameCachePath {
   import context.dispatcher
   import FileCacheImageBrokerActor._
-  val timeout = Timeout(30 seconds)
+  
+  val tempFilePrefix = "ResizingImageDownloader-"
+  implicit val timeout = Timeout(30 seconds)
 
   override def preStart() {
     log.info("Starting FileCacheImageBrokerActor with cache directory %s".format(cachePath))
@@ -50,6 +55,27 @@ class FileCacheImageBrokerActor extends Actor
     case ClearCache() =>
       log.info("Clearing cache directory " + cacheDirectory().getAbsolutePath())
       clearCacheDirectory()
+  }
+  
+  def downloadAndResizeToFile(uri: URI, target: File, preferredSize: Int, format: ImageFormat): Future[Unit] = {
+    val resizeActor = context.actorFor("/user/resizer")
+    val tempFile = File.createTempFile(tempFilePrefix, ".tmp")
+    tempFile.deleteOnExit()
+    for (
+        data <- downloadToFile(uri, tempFile);
+        resizeResponse <- ask(resizeActor, ResizeImageToFileRequest(tempFile, target, preferredSize, format)).mapTo[ResizeImageToFileResponse]
+    ) yield {
+      tempFile.delete()
+      ()
+    }
+  }
+  
+  def downloadToFile(uri: URI, target: File): Future[Long] = {
+    val downloadActor = context.actorFor("/user/downloader")
+    val downloadTask = ask(downloadActor, DownloadToFileRequest(uri, target))
+    for {
+      downloadResponse <- downloadTask.mapTo[DownloadToFileResponse]
+    } yield downloadResponse.fileSize
   }
 
   def handleException(ex: Exception) {
