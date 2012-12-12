23093446c6ecfe202c1d7ec97127ec6f63f51c24
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2012-12-12 15:29:45 +0200

    Upgraded to spray 1.0M6 and Akka 2.0.4

--- src/main/scala/org/ntb/imageresizer/service/ServiceBootstrap.scala
+++ src/main/scala/org/ntb/imageresizer/service/ServiceBootstrap.scala
@@ -1,30 +1,27 @@
 package org.ntb.imageresizer.service
 
-import spray.can.server.HttpServer
-import spray.io._
-import akka.actor._
 import org.ntb.imageresizer.actor.ResizeActor
 import org.ntb.imageresizer.actor.DownloadActor
 import org.ntb.imageresizer.actor.FileCacheImageBrokerActor
+import akka.actor.Props
 import akka.routing.SmallestMailboxRouter
+import spray.can.server.SprayCanHttpServerApp
+import spray.io.IOServer.Unbind
 
-object ServiceBootstrap extends App {
-  val system = ActorSystem("image-resizer")
-  val ioBridge = new IOBridge(system).start()
+object ServiceBootstrap extends App with SprayCanHttpServerApp {
   val resizeActor = system.actorOf(Props[ResizeActor].withRouter(SmallestMailboxRouter(2)), "resizer")
   val downloadActor = system.actorOf(Props[DownloadActor], "downloader")
   val imageBrokerActor = system.actorOf(Props[FileCacheImageBrokerActor].withRouter(SmallestMailboxRouter(2)), "imagebroker")
-  val handler = system.actorOf(Props[ImageResizeServiceActor])
-  val server = system.actorOf(
-    props = Props(new HttpServer(ioBridge, SingletonHandler(handler))),
-    name = "http-server")
-  server ! HttpServer.Bind("localhost", 8080)
+  val service = system.actorOf(Props[ImageResizeServiceActor])
+  val httpServer = newHttpServer(service)
+  httpServer ! Bind(interface = "localhost", port = 8080)
+  
   while (true) {
     Console.print("> ")
     Console.flush()
     val command = Console.readLine()
     if (command == "exit" || command == "quit") {
-      ioBridge.stop()
+      httpServer ! Unbind
       system.shutdown()
       sys.exit()
     }
