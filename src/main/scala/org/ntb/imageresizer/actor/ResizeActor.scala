e2b8de047686fe1f2a744d8339fd2f214eed64c6
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2013-02-25 13:39:13 +0200

    Use actorTry in ResizeActor

--- src/main/scala/org/ntb/imageresizer/actor/ResizeActor.scala
+++ src/main/scala/org/ntb/imageresizer/actor/ResizeActor.scala
@@ -4,29 +4,19 @@ import org.ntb.imageresizer.imageformat.ImageFormat
 import org.ntb.imageresizer.resize.Resizer._
 import org.ntb.imageresizer.resize.UnsupportedImageFormatException
 import akka.actor.Actor
-import akka.actor.ActorLogging
-import akka.actor.Status
-import akka.util.Timeout
-import scala.concurrent.duration._
 import java.io.File
 import language.postfixOps
 
-class ResizeActor extends Actor with ActorLogging {
+class ResizeActor extends Actor with ActorUtils {
   import ResizeActor._
-  implicit val timeout = Timeout(10 seconds)
 
   def receive = {
     case ResizeImageRequest(source, target, size, format) ⇒
-      try {
+      actorTry(sender) {
         resizeImage(source, target, size, format)
-        log.debug("Image resized successfully, replying with ResizeImageResponse()")
         sender ! ResizeImageResponse(target.length())
-      } catch {
+      } actorCatch {
         case e: UnsupportedImageFormatException ⇒
-          sender ! Status.Failure(e)
-        case e: Exception ⇒
-          sender ! Status.Failure(e)
-          throw e
       }
   }
 }
