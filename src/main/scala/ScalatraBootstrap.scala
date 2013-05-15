c0c419336f9392a88beb31d0be7319664fb45524
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2013-05-15 14:47:45 +0300

    Added barebones Scalatra ImageResizeServlet

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,15 +1,18 @@
-import akka.actor.{ActorSystem, Props}
+import _root_.akka.actor.{Props, ActorSystem}
+import _root_.akka.routing.SmallestMailboxRouter
+import javax.servlet.ServletContext
+import org.ntb.imageresizer.actor.file.{FileCacheImageBrokerActor, DownloadActor, ResizeActor}
 import org.ntb.imageresizer.servlet.ImageResizerServlet
 import org.scalatra._
-import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   val system = ActorSystem()
-  //val myActor = system.actorOf(Props[MyActor])
+  val resizeActor = system.actorOf(Props[ResizeActor].withRouter(SmallestMailboxRouter(2)))
+  val downloadActor = system.actorOf(Props[DownloadActor])
+  val imageBrokerActor = system.actorOf(Props(new FileCacheImageBrokerActor(downloadActor, resizeActor)))
 
   override def init(context: ServletContext) {
-    context.mount(new ImageResizerServlet, "/*")
-    //context.mount(new MyActorApp(system, myActor), "/actors/*")
+    context.mount(new ImageResizerServlet(system, imageBrokerActor), "/*")
   }
 
   override def destroy(context:ServletContext) {
--- src/main/scala/org/ntb/imageresizer/servlet/ImageResizerServlet.scala
+++ src/main/scala/org/ntb/imageresizer/servlet/ImageResizerServlet.scala
@@ -1,14 +1,40 @@
 package org.ntb.imageresizer.servlet
 
+import _root_.akka.actor.{ActorSystem, ActorRef}
+import _root_.akka.pattern.ask
+import _root_.akka.util.Timeout
+import java.net.URI
+import language.postfixOps
+import org.ntb.imageresizer.actor.file.FileCacheImageBrokerActor._
+import org.ntb.imageresizer.imageformat._
 import org.scalatra._
-import scalate.ScalateSupport
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 
-class ImageResizerServlet extends ImageResizerStack {
+class ImageResizerServlet(val actorSystem: ActorSystem, val imageBroker: ActorRef) extends ScalatraServlet with FutureSupport {
+  implicit val timeout = Timeout(30 seconds)
+  protected implicit def executor: ExecutionContext = actorSystem.dispatcher
+
+  get("/resize") {
+    val source: String = params("source")
+    val size: Int = params.getOrElse("size", halt(400)).toInt
+    val format: String = params.getOrElse("format", "jpeg")
+    val imageFormat = parseRequestedImageFormat(format).getOrElse(halt(400))
+    contentType = imageFormat.mimeType
+    val req = GetImageRequest(URI.create(source), size, imageFormat)
+    val resizeTask = ask(imageBroker, req).mapTo[GetImageResponse]
+    new AsyncResult { def is =
+      resizeTask.map { response =>
+        Ok(response.data)
+      }
+    }
+  }
+
   get("/") {
     <html>
       <body>
         <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
+        Say hello to Scalate.
       </body>
     </html>
   }
