225d7cd9d214f598418c39d74149aa8b2c5e792b
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2013-06-18 17:31:45 +0300

    Cleaned up FileCacheImageBrokerActorTest

--- src/test/scala/org/ntb/imageresizer/FileCacheImageBrokerActorTest.scala
+++ src/test/scala/org/ntb/imageresizer/FileCacheImageBrokerActorTest.scala
@@ -18,7 +18,7 @@ import java.util.UUID
 import java.util.concurrent.TimeUnit
 import akka.actor.ActorRef
 
-class FileCacheImageBrokerActorTest extends TestKit(ActorSystem("TestSystem")) with ImplicitSender with FlatSpec with ShouldMatchers with BeforeAndAfterAll {
+class FileCacheImageBrokerActorTest extends TestKit(ActorSystem("TestSystem")) with ImplicitSender with FlatSpec with ShouldMatchers {
   import FileCacheImageBrokerActorTest._
 
   val testData: Array[Byte] = Array(1.toByte, 2.toByte, 3.toByte)
@@ -28,43 +28,39 @@ class FileCacheImageBrokerActorTest extends TestKit(ActorSystem("TestSystem")) w
     val testFile = tempFile(testData)
     val downloadActor = TestProbe()
     val resizeActor = TestProbe()
-    val imageBrokerActor = system.actorOf(Props(new TestFileCacheImageBrokerActor(downloadActor.ref, resizeActor.ref, (_ => testFile))))
+    val imageBrokerActor = system.actorOf(Props(new TestFileCacheImageBrokerActor(downloadActor.ref, resizeActor.ref, _ ⇒ testFile)))
     imageBrokerActor ! GetImageRequest(new URI("http://localhost/file1.png"), 200)
     expectMsgPF(timeout) {
-      case GetImageResponse(data) => data.getAbsolutePath should equal(testFile.getAbsolutePath)
+      case GetImageResponse(data) ⇒ data.getAbsolutePath should equal(testFile.getAbsolutePath)
     }
-    testFile.delete()
     system.stop(imageBrokerActor)
+    testFile.delete()
   }
 
   it should "download and resize nonexisting file" in {
     val testFile = nonExistingFile()
     val downloadProbe = TestProbe()
     val resizeProbe = TestProbe()
-    val imageBrokerActor = system.actorOf(Props(new TestFileCacheImageBrokerActor(downloadProbe.ref, resizeProbe.ref, (_ => testFile))))
+    val imageBrokerActor = system.actorOf(Props(new TestFileCacheImageBrokerActor(downloadProbe.ref, resizeProbe.ref, _ ⇒ testFile)))
     imageBrokerActor ! GetImageRequest(new URI("http://localhost/file2.png"), 200)
     downloadProbe.expectMsgPF(timeout) {
-      case DownloadRequest(uri, target) =>
+      case DownloadRequest(uri, target) ⇒
         Files.write(testData, target)
         downloadProbe.reply(DownloadResponse(target, target.length()))
     }
     resizeProbe.expectMsgPF(timeout) {
-      case ResizeImageRequest(source, target, _, _) =>
+      case ResizeImageRequest(source, target, _, _) ⇒
         Files.copy(source, target)
         resizeProbe.reply(ResizeImageResponse(target.length()))
     }
     expectMsgPF(timeout) {
-      case GetImageResponse(data) =>
+      case GetImageResponse(data) ⇒
         testFile should be('exists)
         Files.toByteArray(testFile) should equal(testData)
         data.getAbsolutePath should equal(testFile.getAbsolutePath)
     }
-    testFile.delete()
     system.stop(imageBrokerActor)
-  }
-
-  override def afterAll() {
-    system.shutdown()
+    testFile.delete()
   }
 }
 
@@ -86,7 +82,7 @@ object FileCacheImageBrokerActorTest {
     tempFile
   }
 
-  class TestFileCacheImageBrokerActor(downloader: ActorRef, resizer: ActorRef, provider: Key => File) extends FileCacheImageBrokerActor(downloader, resizer) {
+  class TestFileCacheImageBrokerActor(downloader: ActorRef, resizer: ActorRef, provider: Key ⇒ File) extends FileCacheImageBrokerActor(downloader, resizer) {
     override val cacheFileProvider = provider
   }
 }
