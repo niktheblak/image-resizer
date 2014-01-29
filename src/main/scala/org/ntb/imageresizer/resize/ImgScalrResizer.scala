65db635fda8e714df08279f94c9acd5875a2a206
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2014-01-29 11:57:51 +0200

    Couple of extract methods in ImgScalrResizer

--- src/main/scala/org/ntb/imageresizer/resize/ImgScalrResizer.scala
+++ src/main/scala/org/ntb/imageresizer/resize/ImgScalrResizer.scala
@@ -1,14 +1,11 @@
 package org.ntb.imageresizer.resize
 
-import org.ntb.imageresizer.imageformat.ImageFormat
-
-import org.imgscalr.Scalr
-
 import java.awt.image.BufferedImage
 import java.io.OutputStream
-
 import javax.imageio.ImageIO
+import org.imgscalr.Scalr
 import org.imgscalr.Scalr.Mode
+import org.ntb.imageresizer.imageformat.ImageFormat
 
 trait ImgScalrResizer {
   def resizeBufferedImage(image: BufferedImage, output: OutputStream, size: Int, format: ImageFormat) {
@@ -18,20 +15,25 @@ trait ImgScalrResizer {
   }
 
   def resizeBufferedImage(image: BufferedImage, output: OutputStream, width: Int, height: Int, format: ImageFormat) {
-    usingImage(widthAndHeightResize(image, width, height)) { scaled ⇒
+    usingImage(resizeBasedOnWidthAndHeight(image, width, height)) { scaled ⇒
       ImageIO.write(scaled, format.extension, output)
     }
   }
 
-  def widthAndHeightResize(image: BufferedImage, width: Int, height: Int): BufferedImage =
-    if (width > 0 && height > 0)
-      Scalr.resize(image, Mode.FIT_EXACT, width, height)
-    else if (width <= 0)
-      Scalr.resize(image, Mode.FIT_TO_HEIGHT, 0, height)
-    else if (height <= 0)
-      Scalr.resize(image, Mode.FIT_TO_WIDTH, width, 0)
-    else
-      throw new IllegalArgumentException("Width or height must be above zero")
+  def resizeBasedOnWidthAndHeight(image: BufferedImage, width: Int, height: Int): BufferedImage =
+    if (width > 0 && height > 0) fitExact(image, width, height)
+    else if (width <= 0) fitToHeight(image, height)
+    else if (height <= 0) fitToWidth(image, width)
+    else throw new IllegalArgumentException("Width or height must be above zero")
+
+  def fitToWidth(image: BufferedImage, width: Int): BufferedImage =
+    Scalr.resize(image, Mode.FIT_TO_WIDTH, width, 0)
+
+  def fitToHeight(image: BufferedImage, height: Int): BufferedImage =
+    Scalr.resize(image, Mode.FIT_TO_HEIGHT, 0, height)
+
+  def fitExact(image: BufferedImage, width: Int, height: Int): BufferedImage =
+    Scalr.resize(image, Mode.FIT_EXACT, width, height)
 
   def usingImage[R](c: BufferedImage)(action: BufferedImage ⇒ R): R = {
     try {
