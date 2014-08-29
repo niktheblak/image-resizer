f527ca5297232a30a8ae34c2b2728b4f3748cb14
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2014-08-29 13:19:18 +0300

    Use DefaultHasher for generating image IDs

--- src/main/scala/org/ntb/imageresizer/actor/KeyEncoder.scala
+++ src/main/scala/org/ntb/imageresizer/actor/KeyEncoder.scala
@@ -1,20 +1,15 @@
 package org.ntb.imageresizer.actor
 
-import com.google.common.hash.Hashing
-import com.google.common.io.BaseEncoding
 import org.ntb.imageresizer.imageformat.ImageFormat
 import org.ntb.imageresizer.storage.FormatEncoder
+import org.ntb.imageresizer.util.DefaultHasher
 
-trait KeyEncoder extends FormatEncoder {
-  private val hashFunction = Hashing.goodFastHash(128)
-  private val encoding = BaseEncoding.base64Url().omitPadding()
-
+trait KeyEncoder extends FormatEncoder with DefaultHasher {
   def encodeKey(source: String, size: Int, format: ImageFormat): String = {
     val hasher = hashFunction.newHasher()
     hasher.putUnencodedChars(source)
     hasher.putInt(size)
     hasher.putByte(formatToByte(format))
-    val hash = hasher.hash()
-    encoding.encode(hash.asBytes())
+    hasher.hash().toString
   }
 }
