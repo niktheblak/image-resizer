6df340c1e9ce41698d3cc7ded927ba2b5f1d8d47
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2014-08-29 13:15:08 +0300

    Use better hash function in generating filenames

--- src/main/scala/org/ntb/imageresizer/util/DefaultHasher.scala
+++ src/main/scala/org/ntb/imageresizer/util/DefaultHasher.scala
@@ -3,7 +3,7 @@ package org.ntb.imageresizer.util
 import com.google.common.hash.Hashing
 
 trait DefaultHasher {
-  val hashFunction = Hashing.md5()
+  val hashFunction = Hashing.goodFastHash(128)
 
   def hashBytes(input: Array[Byte]): String = hashFunction.hashBytes(input).toString
 
