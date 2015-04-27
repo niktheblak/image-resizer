ab8ad0f171b17d5d56d57f433c8d5a6e20a88842
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2015-04-27 17:26:40 +0300

    Added unit tests for ResizeActor

---
+++ src/test/scala/org/ntb/imageresizer/actor/ResizeActorTest.scala
@@ -0,0 +1,59 @@
+package org.ntb.imageresizer.actor
+
+import java.io.File
+
+import akka.actor.{Status, ActorSystem}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import akka.util.ByteString
+import org.ntb.imageresizer.imageformat._
+import org.ntb.imageresizer.io.ByteStringIO
+import org.ntb.imageresizer.resize.ResolutionTool
+import org.scalatest._
+
+import scala.concurrent.duration._
+
+class ResizeActorTest
+    extends TestKit(ActorSystem("TestActorSystem"))
+    with ImplicitSender
+    with FlatSpecLike
+    with Matchers
+    with BeforeAndAfter
+    with BeforeAndAfterAll {
+  import ResizeActor._
+
+  val sampleImage = new File("src/test/resources/test_image.jpeg")
+  val testTimeout = 2.seconds
+  var resizeActor: TestActorRef[ResizeActor] = _
+
+  override def afterAll() {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  before {
+    resizeActor = TestActorRef(new ResizeActor)
+  }
+
+  after {
+    resizeActor.stop()
+  }
+
+  "ResizeActor" should "resize sample image to requested dimensions" in {
+    val source = ByteStringIO.read(sampleImage)
+    resizeActor ! ResizeImageRequest(source, 200, JPEG)
+    expectMsgPF(testTimeout) {
+      case ResizeImageResponse(data) ⇒
+        val (width, height) = ResolutionTool.readResolution(data)
+        width shouldEqual 118
+        height shouldEqual 200
+    }
+  }
+
+  it should "throw with invalid image data" in {
+    val source = ByteString(1, 2, 3)
+    resizeActor ! ResizeImageRequest(source, 200, JPEG)
+    expectMsgPF(testTimeout) {
+      case Status.Failure(t) ⇒
+        t.getMessage should include ("Failed to decode image")
+    }
+  }
+}
