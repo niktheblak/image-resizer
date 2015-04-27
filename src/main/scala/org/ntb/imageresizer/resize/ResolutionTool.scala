a93006c85dc8d4ea3e11ae616b417b61830272b8
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2015-04-27 17:26:19 +0300

    Added utility class for getting resolution of images

---
+++ src/main/scala/org/ntb/imageresizer/resize/ResolutionTool.scala
@@ -0,0 +1,25 @@
+package org.ntb.imageresizer.resize
+
+import java.awt.image.BufferedImage
+import java.io.{InputStream, File}
+import java.net.URL
+
+import akka.util.ByteString
+import org.ntb.imageresizer.resize.Resizer._
+
+object ResolutionTool extends JavaImageIOImageReader {
+  def readResolution(input: InputStream): (Int, Int) =
+    usingImage(read(input))(getResolution)
+
+  def readResolution(input: File): (Int, Int) =
+    usingImage(read(input))(getResolution)
+
+  def readResolution(input: URL): (Int, Int) =
+    usingImage(read(input))(getResolution)
+
+  def readResolution(input: ByteString): (Int, Int) =
+    usingImage(read(input))(getResolution)
+
+  def getResolution(image: BufferedImage): (Int, Int) =
+    (image.getWidth, image.getHeight)
+}
