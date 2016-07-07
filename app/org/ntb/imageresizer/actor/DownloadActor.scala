4d2cc48f7611b8a744472569631b6cc83e71b540
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2016-07-07 15:06:37 +0300

    Fixed handling of invalid URLs in DownloadActor

--- app/org/ntb/imageresizer/actor/DownloadActor.scala
+++ app/org/ntb/imageresizer/actor/DownloadActor.scala
@@ -19,17 +19,23 @@ class DownloadActor @Inject() (ws: WSClient) extends Actor with ActorUtils {
   def receive = {
     case DownloadRequest(url) ⇒
       Logger.debug(s"Downloading image from $url")
-      val request = ws
-        .url(url)
-        .withRequestTimeout(10.seconds)
-      request.get() map { r =>
-        if (r.status != Status.OK) {
-          throw new DownloadException(r.status, r.statusText, r.body)
-        }
-        val data = r.bodyAsBytes
-        Logger.debug(s"Downloaded ${data.length} bytes of image with type ${r.header(HeaderNames.CONTENT_TYPE).getOrElse("unknown")} from $url")
-        DownloadResponse(data)
-      } pipeTo sender
+      try {
+        validateUrl(url)
+        val request = ws
+          .url(url)
+          .withRequestTimeout(10.seconds)
+        request.get() map { r =>
+          if (r.status != Status.OK) {
+            throw new DownloadException(r.status, r.statusText, r.body)
+          }
+          val data = r.bodyAsBytes
+          Logger.debug(s"Downloaded ${data.length} bytes of image with type ${r.header(HeaderNames.CONTENT_TYPE).getOrElse("unknown")} from $url")
+          DownloadResponse(data)
+        } pipeTo sender()
+      } catch {
+        // ws methods can throw before the future is created
+        case e: Exception => sender() ! akka.actor.Status.Failure(e)
+      }
   }
 }
 
@@ -37,5 +43,16 @@ object DownloadActor {
   case class DownloadRequest(url: String)
   case class DownloadResponse(data: ByteString)
   class DownloadException(status: Int, statusText: String, body: String) extends RuntimeException(s"Server responded with HTTP $status $statusText: $body")
+
+  class InvalidUrlException(msg: String, cause: Throwable) extends RuntimeException(msg, cause)
+
+  def validateUrl(url: String) {
+    try {
+      org.asynchttpclient.uri.Uri.create(url)
+    } catch {
+      case e: NullPointerException => throw new InvalidUrlException("URL segment is missing", e)
+      case e: IllegalArgumentException => throw new InvalidUrlException("Invalid URL", e)
+    }
+  }
 }
 
