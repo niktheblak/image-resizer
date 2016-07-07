6298b9baab941de957f4f2a8c519c85577f6f8d6
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2016-07-07 14:42:09 +0300

    Added support for resizing from image data in POST body

---
+++ app/org/ntb/imageresizer/service/FileHasher.scala
@@ -0,0 +1,15 @@
+package org.ntb.imageresizer.service
+
+import java.io.File
+
+import com.google.common.hash.Hashing
+import com.google.common.io.Files
+
+trait FileHasher {
+  val hashFunction = Hashing.goodFastHash(128)
+
+  def hash(file: File): String = {
+    val hashCode = Files.hash(file, hashFunction)
+    hashCode.toString
+  }
+}
--- app/org/ntb/imageresizer/service/ImageResizerController.scala
+++ app/org/ntb/imageresizer/service/ImageResizerController.scala
@@ -14,7 +14,7 @@ import scala.concurrent.Future
 import scala.concurrent.duration._
 
 @Singleton
-class ImageResizerController @Inject() (@Named("imagebroker") imageBroker: ActorRef) extends Controller {
+class ImageResizerController @Inject() (@Named("imagebroker") imageBroker: ActorRef) extends Controller with FileHasher {
   import play.api.libs.concurrent.Execution.Implicits._
   implicit val timeout: Timeout = 10.seconds
 
@@ -23,7 +23,38 @@ class ImageResizerController @Inject() (@Named("imagebroker") imageBroker: Actor
     val request = GetImageRequest(source, size, imageFormat)
     val resizeTask = ask(imageBroker, request).mapTo[GetImageResponse]
     resizeTask.map { r =>
-      Ok(r.data).as(imageFormat.mimeType)
+      val location = s"/resize?source=$source&size=$size&format=$format"
+      Ok(r.data)
+        .as(imageFormat.mimeType)
+        .withHeaders(LOCATION -> location)
+    } recoverWith {
+      case e: IllegalArgumentException => Future(BadRequest(e.getMessage))
+      case e: DownloadException => Future(BadGateway(e.getMessage))
+      case e: RuntimeException => Future(InternalServerError(e.getMessage))
+    }
+  }
+
+  def resizeFromBody(id: Option[String], size: Int, format: String) = Action.async(parse.temporaryFile) { request =>
+    val fmt = for {
+      t <- request.headers.get(CONTENT_TYPE)
+      f <- parseImageFormatFromMimeType(t)
+    } yield f
+    val imageFormat = fmt.getOrElse(JPEG)
+    val body = request.body
+    val finalId = id match {
+      case Some(i) => i
+      case None => hash(body.file)
+    }
+    val message = GetLocalImageRequest(body.file, finalId, size, imageFormat)
+    val resizeTask = ask(imageBroker, message).mapTo[GetImageResponse]
+    resizeTask onComplete { _ =>
+      body.clean()
+    }
+    resizeTask.map { r =>
+      val location = s"/resize?source=$finalId&size=$size&format=$format"
+      Ok(r.data)
+        .as(imageFormat.mimeType)
+        .withHeaders(LOCATION -> location)
     } recoverWith {
       case e: IllegalArgumentException => Future(BadRequest(e.getMessage))
       case e: DownloadException => Future(BadGateway(e.getMessage))
