3fb352ffda22e7d2b39eac406c301cd49e2d627d
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2016-10-26 17:23:40 +0300

    Formatted source code

--- app/Module.scala
+++ app/Module.scala
@@ -3,7 +3,7 @@ import javax.imageio.ImageIO
 import akka.actor.Props
 import akka.routing.BalancingPool
 import com.google.inject.AbstractModule
-import org.ntb.imageresizer.actor.{ImageBrokerActor, ResizeActor}
+import org.ntb.imageresizer.actor.{ ImageBrokerActor, ResizeActor }
 import play.api.libs.concurrent.AkkaGuiceSupport
 
 class Module extends AbstractModule with AkkaGuiceSupport {
