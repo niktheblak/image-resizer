8c6fc8328108b12fb97d169552119821d8d4fe22
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2017-08-10 17:53:53 +0300

    Changed default hash function to murmur128

--- app/org/ntb/imageresizer/util/DefaultHasher.scala
+++ app/org/ntb/imageresizer/util/DefaultHasher.scala
@@ -3,7 +3,7 @@ package org.ntb.imageresizer.util
 import com.google.common.hash.{ HashFunction, Hashing }
 
 trait DefaultHasher {
-  val hashFunction: HashFunction = Hashing.md5()
+  val hashFunction: HashFunction = Hashing.murmur3_128()
 
   def hashBytes(input: Array[Byte]): String = hashFunction.hashBytes(input).toString
 
